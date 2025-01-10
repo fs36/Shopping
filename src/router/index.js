2d14153b45670cb0bb12928b51da73d3c917b075
zxw <1935661782@qq.com>
2025-01-11 00:12:56 +0800

    finish tabber

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,33 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
 import Layout from '@/views/layout'
-import Search from '@/views/search'
-import SearchList from '@/views/searchlist'
+import Search from '@/views/search/search'
+import SearchList from '@/views/search/list'
 import ProDetail from '@/views/prodetail'
 import Pay from '@/views/pay'
 import MyOrder from '@/views/myorder'
+
+import Home from '@/views/layout/home'
+import Category from '@/views/layout/category'
+import Cart from '@/views/layout/cart'
+import User from '@/views/layout/user'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', component: Login },
+    {
+      path: '/',
+      component: Layout,
+      redirect: '/home',
+      children: [
+        { path: '/home', component: Home },
+        { path: '/category', component: Category },
+        { path: '/cart', component: Cart },
+        { path: '/user', component: User }
+      ]
+    },
     { path: '/', component: Layout },
     { path: '/search', component: Search },
     { path: '/searchlist', component: SearchList },
